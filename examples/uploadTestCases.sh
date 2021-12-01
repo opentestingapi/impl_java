ed834ec7a94a14449bc1aef055bbf29a71bdb6f4
Robert Diers <robert.diers@accenture.com>
2021-12-01 14:29:31 +0000

    validation fix

--- examples/uploadTestCases.sh
+++ examples/uploadTestCases.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 # define e2etest server
-#e2etestserver=http://localhost:50000
-e2etestserver=http://hofva9062pnrt.atisasg.com:8080
-echo ${e2etestserver}
+opentestingserver=http://localhost:50000
+#opentestingserver=http://hofva9062pnrt.atisasg.com:8080
+echo ${opentestingserver}
 
 # upload required files and test case
 
 #for testcase in local_test_00001 #Mix
-#for testcase in local_test_00002 #Kafka
+for testcase in local_test_00002 #Kafka
 #for testcase in local_test_00003 #Cassandra
-for testcase in local_test_00004 #Rest
+#for testcase in local_test_00004 #Rest
 #for testcase in local_test_00005 #Oracle
 #for testcase in local_test_00006 #PostGre
 #for testcase in local_test_00001 local_test_00002 local_test_00003 local_test_00004
@@ -19,8 +19,8 @@ do
     cd ${testcase}
     for f in *
     do
-        curl -X POST "${e2etestserver}/upload/file/${testcase}/${f}" -H "accept: */*" -H "Content-Type: application/json" -d @${f} && echo  
+        curl -X POST "${opentestingserver}/upload/file/${testcase}/${f}" -H "accept: */*" -H "Content-Type: application/json" -d @${f} && echo  
     done
     cd ..
-    curl -X POST "${e2etestserver}/upload/test" -H "accept: */*" -H "Content-Type: application/json" -d @${testcase}.json && echo
+    curl -X POST "${opentestingserver}/upload/test" -H "accept: */*" -H "Content-Type: application/json" -d @${testcase}.json && echo
 done
