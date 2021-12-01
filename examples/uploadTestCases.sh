479a43b5a9b16367e9a8cdf83bb7c141a2b09ac4
Robert Diers <robert.diers@accenture.com>
2021-12-01 16:44:53 +0000

    hello world example

--- examples/uploadTestCases.sh
+++ examples/uploadTestCases.sh
@@ -7,20 +7,23 @@ echo ${opentestingserver}
 
 # upload required files and test case
 
-for testcase in local_test_00001 #Mix
+#for testcase in local_test_00001 #Mix
 #for testcase in local_test_00002 #Kafka
 #for testcase in local_test_00003 #Cassandra
-#for testcase in local_test_00004 #Rest
+for testcase in local_test_00004 #Rest
 #for testcase in local_test_00005 #Oracle
 #for testcase in local_test_00006 #PostGre
 #for testcase in local_test_00001 local_test_00002 local_test_00003 local_test_00004
+#for testcase in local_test_helloworld #hello world, but I will do nothing...
 do  
     echo "### ${testcase}:"
-    cd ${testcase}
-    for f in *
-    do
-        curl -X POST "${opentestingserver}/upload/file/${testcase}/${f}" -H "accept: */*" -H "Content-Type: application/json" -d @${f} && echo  
-    done
-    cd ..
+    if [ -d "${testcase}" ]; then
+        cd ${testcase}
+        for f in *
+        do
+            curl -X POST "${opentestingserver}/upload/file/${testcase}/${f}" -H "accept: */*" -H "Content-Type: application/json" -d @${f} && echo  
+        done
+        cd ..
+    fi
     curl -X POST "${opentestingserver}/upload/test" -H "accept: */*" -H "Content-Type: application/json" -d @${testcase}.json && echo
 done
