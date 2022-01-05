ae3f6985eaf367eb324c6f8825ec06b054833a78
Robert Diers <robert.diers@accenture.com>
2022-01-05 09:21:25 +0000

    mig fixes

--- examples/api0_1/uploadTestCases.sh
+++ examples/api0_1/uploadTestCases.sh
@@ -6,8 +6,8 @@ echo ${opentestingserver}
 
 # upload required files and test case
 
-#for testcase in local_test_00001 #Mix
-for testcase in local_test_00002 #Kafka
+for testcase in local_test_00001 #Mix
+#for testcase in local_test_00002 #Kafka
 #for testcase in local_test_00003 #Cassandra
 #for testcase in local_test_00004 #Rest
 #for testcase in local_test_00005 #Oracle
