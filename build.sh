1f88d57856e07015fc5621f8c8a8a5b07623b503
Robert Diers <robert.diers@accenture.com>
2022-11-28 08:44:45 +0100

    remove jib exec

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-# build app (podman used for Docker image creation, for Docker env you can remove the executable flag)
-mvn clean install jib:dockerBuild -Djib.dockerClient.executable=$(which podman)
-#mvn clean install jib:dockerBuild
+# build app
+mvn clean install
