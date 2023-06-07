e61395a109a6397bae2870db83bf0c559ef25612
Robert Diers <robert.diers@accenture.com>
2023-06-07 14:14:44 +0200

    Sonar fixes

--- sonar.sh
+++ sonar.sh
@@ -1,2 +1,2 @@
 # run Sonar - please create and replace login token first
-mvn sonar:sonar -Dsonar.host.url=http://localhost:9001 -Dsonar.login=a197bccb64bb53ce9ab5ebb31aa81a21b68625cb -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dsonar.java.binaries=.
+mvn sonar:sonar -Dsonar.host.url=http://localhost:9001 -Dsonar.login=sqa_11d2b870619c09c11c11ec071d4e30a5cf7b7f51 -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true -Dsonar.java.binaries=.
