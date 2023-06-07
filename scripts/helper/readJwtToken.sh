b1529077558353d6577d9b3fb35e2f51d5add0a4
Robert Diers <robert.diers@accenture.com>
2023-06-07 16:04:15 +0200

    add Keycloak test case

--- scripts/helper/readJwtToken.sh
+++ scripts/helper/readJwtToken.sh
@@ -1,11 +1,10 @@
-#test script to get JWT from keycloak instance, user "robert" and client "XXX" (public) must be created first
 
-#curl -L -X POST 'http://localhost:8080/auth/realms/dcm4che/protocol/openid-connect/token' \
+#curl -L -X POST 'http://localhost:8080/realms/master/protocol/openid-connect/token' \
 #-H 'Content-Type: application/x-www-form-urlencoded' \
-#--data-urlencode 'client_id=XXX' \
+#--data-urlencode 'client_id=admin-cli' \
 #--data-urlencode 'grant_type=password' \
-#--data-urlencode 'username=robert' \
+#--data-urlencode 'username=admin' \
 #--data-urlencode 'password=admin'
 
-curl -L -X POST 'http://localhost:8080/auth/realms/dcm4che/protocol/openid-connect/token' \
--d "client_id=XXX&grant_type=password&username=robert&password=admin"
+curl -L -X POST 'http://localhost:8080/realms/master/protocol/openid-connect/token' \
+-d "client_id=admin-cli&grant_type=password&username=admin&password=admin"
